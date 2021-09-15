2c4c84974611377ff383a56a17f0547408204f24
namvkim <sonnamttt@gmail.com>
2021-09-15 19:52:14 +0700

    a

---
+++ vue_html/index.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Do you wanna build a Vue app?'
+    },
+    method: {
+        toggleShowMessage() {
+            console.log("aaaaaaaaaa")
+        },
+    },
+})
